34f0b3cf2b1da97a3e53e54d486c23dd07473150
EugeniaBD88 <122355951+EugeniaBD88@users.noreply.github.com>
2025-01-03 22:15:20 +0000

    Initial commit

--- firebase/config.ts
+++ firebase/config.ts
@@ -5,16 +5,28 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBs7EOyYqAXc3B-fprQZyF5MveLsuhbE4s",
+  authDomain: "socialevent-6ecc2.firebaseapp.com",
+  projectId: "socialevent-6ecc2",
+  storageBucket: "socialevent-6ecc2.firebasestorage.app",
+  messagingSenderId: "193082872516",
+  appId: "1:193082872516:web:2765615dcd3a7a0d3d6f91",
+  measurementId: "G-GSBWPTGDKQ",
+  databaseURL:  "https://socialevent-6ecc2-default-rtdb.firebaseio.com/",
+
+
+};
 // Initialize Firebase
 const firebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
